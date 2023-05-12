7c7b8b8404f7cc115578865353f952f07c4726be
KrasniKot <emanueltrias9@gmail.com>
2023-05-12 07:15:54 -0700

    betty fixed

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,18 +8,18 @@
 */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
 
-    if (!new)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
+	if (!new)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
-    new->n = value;
-    new->left = NULL;
-    new->right = NULL;
-    new->parent = parent;
+	new->n = value;
+	new->left = NULL;
+	new->right = NULL;
+	new->parent = parent;
 
-    return (new);
+	return (new);
 }
