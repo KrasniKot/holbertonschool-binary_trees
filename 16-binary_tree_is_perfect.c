2d5dae187499f9d064b84f0e4f275b96072bb235
KrasniKot <6187@holbertonstudents.com>
2023-05-15 11:25:07 -0700

    binary tree height fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	lh = tree->left ? 1 + binary_tree_height(tree->left) : +0;
-	rh = tree->right ? 1 + binary_tree_height(tree->right) : +0;
+	lh = tree->left ? 1 + binary_tree_height(tree->left) : +1;
+	rh = tree->right ? 1 + binary_tree_height(tree->right) : +1;
 
 	return ((lh > rh) ? lh : rh);
 
